7883115df61b74feadd20a104e0234c250b4aee1
Aidan <me@aidantaylor.net>
2016-04-25 21:07:59 +0100

    removed config

--- src/main/java/net/aidantaylor/bukkit/realping/RealPing.java
+++ src/main/java/net/aidantaylor/bukkit/realping/RealPing.java
@@ -11,7 +11,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RealPing extends JavaPlugin implements Listener {
 	private boolean debug = true;
-	private FileConfiguration configFile;
 
 	@Override
 	public void onEnable() {
